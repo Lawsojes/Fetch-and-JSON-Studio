bd832fcbe2a06ae743096c7f609ca3641c12648e
Jesse Lawson <jesse.t.lawson@gmail.com>
2023-07-27 18:33:13 -0500

    added to script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // TODO: add code here
+
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(respnse){
+        respnse.json().then(function(json){
+            console.log(json)
+            let output = document.getElementById("container")
+            for (let key in json){
+                output.innerHTML =`
+                <div>
+                    <div>
+                    <h3>${json.firstName} ${json.lastName}</h3>
+                    <ul>
+
+                    </ul>
+                    </div>
+                </div>
+                `
+            }
+      
+      
+        })
+    })
+
+})
